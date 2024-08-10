3e98bb86d6d09732314d3e4dfe5123180fcbd6ce
ChioGT <chio.gonzalezt@gmail.com>
2024-08-10 16:26:52 -0500

    Add DeterministicIdentity

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 import express from 'express';
+import { Identity, Group } from '@semaphore-protocol/core';
 import fs from 'fs';
 
 const app = express();
+
+const newIdentity = (secret) => {
+    const deterministicIdentity = new Identity(toString(secret));
+    console.log("Deterministic Identity: ", deterministicIdentity);
+};
+
+const newGroup = () => {
+    // array of members to add to the group
+    const members = [];
+    for (let x=0; x < 5; x++){
+        let ide = newIdentity(toString(x));
+        members.push(ide("_commitment"));
+    }; 
+    const group = new Group(members);
+    console.log("Group size: ", group.members.length);
+};
+
+newIdentity("secreto");
 
 const readData = () => {
     try{
@@ -12,6 +31,8 @@ const readData = () => {
     };    
    
 };
+
+
 
 const writeData = () => {
     try{
